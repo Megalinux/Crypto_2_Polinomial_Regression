3a3a083ddfc1baf6e451ac9d7be0d2d0a0030bda
Megalinux <grando.ruggero@gmail.com>
2021-12-29 09:47:52 +0100

    Update crypto_2_polinomial_regression_v1.py
    Add the visualization of predict values

--- crypto_2_polinomial_regression_v1.py
+++ crypto_2_polinomial_regression_v1.py
@@ -5,7 +5,7 @@ import ccxt
 import calendar
 from datetime import datetime
 from sympy import Symbol,expand
-import tools as tl
+import Tools as tl
 
 #Set parameters of Matplotlib
 np.random.seed(100)
@@ -49,20 +49,22 @@ plt.show()
 reg = np.polyfit(f,l,deg=5)
 equazione = np.poly1d(reg)
 x=Symbol('x')
+#print function with coefficients
 print(expand(equazione(x)))
 
 #Calculate the approximate functions of the third, fifth and seventh degree
-p = np.polyval(reg,f) 
-reg1 = np.polyfit(f,l,deg=3) 
-reg2 = np.polyfit(f,l,deg=7) 
-p1 = np.polyval(reg1,f) 
-p2 = np.polyval(reg2,f) 
+p = np.polyval(reg,f) #valori predetti di quinto grado 
+reg1 = np.polyfit(f,l,deg=3) #coefficienti funzione di terzo grado
+reg2 = np.polyfit(f,l,deg=7) #coefficienti funzione di settimo grado
+p1 = np.polyval(reg1,f) #valori predetti di terzo grado
+p2 = np.polyval(reg2,f) #valori predetti di settimo grado
 
-#Print MSE values
+
 tl1 = tl.Tools()
-print('%.12f' % tl1.MSE(l,p)) 
-print('%.12f' % tl1.MSE(l,p1)) 
-print('%.12f' % tl1.MSE(l,p2)) 
+#Print MSE values
+print('%.12f' % tl1.MSE(l,p1)) #terzo grado
+print('%.12f' % tl1.MSE(l,p)) #quinto grado
+print('%.12f' % tl1.MSE(l,p2)) #settimo grado
 
 #Displays the approximation functions
 plt.figure(figsize=(10,6))
@@ -76,15 +78,53 @@ plt.legend()
 #Set the labels in the fifth degree function
 """for a,b in zip(f, p2): 
     plt.text(a, b, "{:.12f}".format(b))"""
+
 plt.show()
 
-#Calculate the step of 15 minute for the future value
-c = 2 / f.size
-d = 2+c
+#Calculate the step of 5 minute for the future value
+c = 2*2 / f.size #measure of 5 minute; 2 (remember size -2 to +2) equal max = last value of time
+d = 2+c #last value of time plus c 
 
-#Calcolate the future value (in this case 15 minute) of the seventh degree function.
+#add value d to numpy array f1
+f1= np.append(f,d)
+#add len(l)-1 to numpy array l1
+l1 = np.append(l,l[len(l)-1])
+
+
+#Calcolate the future value (in this case 5 minute) 
+predetto5grado = np.polyval(reg,d)
+predetto3grado = np.polyval(reg1,d)
 predetto7grado = np.polyval(reg2,d)
-print('Future 15 minute value of price ETH/BTC: ')
+
+#add predict value to numpy array of regression degree on last position
+p1p = np.append(p1,predetto3grado)
+p2p = np.append(p2,predetto7grado)
+pp = np.append(p,predetto5grado)
+print('*********************')
+print('Predict values: ')
+print('*********************')
+print('%.12f' % predetto3grado) 
+print('%.12f' % predetto5grado) 
 print('%.12f' % predetto7grado) 
 
+#print of regression degree + predict value
+plt.plot(f1,l1,'ro',label='real values')
+plt.plot(f1,p1p,'--',label='regression 3 degree + predict value')
+plt.plot(f1,pp,'--',label='regression 5 degree + predict value')
+plt.plot(f1,p2p,'--',label='regression 7 degree + predict value')
+plt.legend()
+plt.show()
 
+#trasform the predict value in price value
+print(np.std(l1))
+cl = df1_risultato['Close'].values
+price3grado = (float(predetto3grado) * float(np.std(cl))) +float(cl.mean())
+price5grado = (float(predetto5grado) * float(np.std(cl))) +float(cl.mean())
+price7grado = (float(predetto7grado) * float(np.std(cl))) +float(cl.mean())
+
+print('*********************')
+print('Price values: ')
+print('*********************')
+print(price3grado)
+print(price5grado)
+print(price7grado)
